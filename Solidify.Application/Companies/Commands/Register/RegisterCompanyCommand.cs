c2f94449d479b08244c8413beb822afa8e88924d
moustafanafeaa <moustafa.anafea@gmail.com>
2025-01-23 12:48:04 +0200

    add register to company

---
+++ Solidify.Application/Companies/Commands/Register/RegisterCompanyCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Solidify.Application.Accounts.Commands.Register;
+using Solidify.Application.Common.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solidify.Application.Companies.Commands.Register
+{
+    public class RegisterCompanyCommand : RegisterUserCommand ,IRequest<GeneralResponseDto>
+    {
+        [MinLength(3)]
+        [Required]
+        public string CompanyName { get; set; }
+
+     //   public string Address { get; set; }
+        public string CommericalNumber { get; set; }
+        public long TaxId { get; set; }
+        public IFormFile CommericalLicense { get; set; }
+        public string? CompanyWebSite { get; set; }
+        public string? FaceBookAccout { get; set; }
+        public string? InstagramAccount { get; set; }
+        public string? TwitterAccount { get; set; }
+        public string BankAccount { get; set; }
+        public string PaymentTerm { get; set; }
+      //  public string UserName { get; set; }
+      //  public string Email { get; set; }
+     //   public string Password { get; set; }
+       // [Compare("Password")]
+       // [Required]
+       // [DataType(DataType.Password)]
+       //// public string ConfirmPassword { get; set; }
+       // public string PhoneNumber { get; set; }
+
+    }
+}
---
+++ Solidify.Application/Companies/Commands/Register/RegisterCompanyCommandHandler.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Solidify.Application.Common.Dtos;
+using Solidify.Application.Files;
+using Solidify.Domain.Entities;
+using Solidify.Domain.Interfaces;
+using static Solidify.Application.Common.GeneralResponse;
+
+namespace Solidify.Application.Companies.Commands.Register
+{
+    public class RegisterCompanyCommandHandler(UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager
+        , IFileService fileService
+        ,ICompanyRepository companyRepository
+        ) : IRequestHandler<RegisterCompanyCommand, GeneralResponseDto>
+    {
+        public async Task<GeneralResponseDto> Handle(RegisterCompanyCommand request, CancellationToken cancellationToken)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                Address = request.Address,
+            };
+            var result = await userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+            {
+                return CreateResponse(false, 400, result.Errors.Select(e => e.Description).ToList(), "Registration failed");
+            }
+
+            var licenseUploadResult = await fileService.UploadFileAsync(request.CommericalLicense, "License");
+            if (!licenseUploadResult.IsSucceeded)
+            {
+                await userManager.DeleteAsync(user);
+                return licenseUploadResult;
+            }
+
+            var company = new Company
+            {   
+                Id = user.Id,
+                CompanyName = request.CompanyName,
+                BankAccount = request.BankAccount,
+                CompanyWebSite = request.CompanyWebSite,
+                CommericalLicense = licenseUploadResult.Model.ToString(),
+                CommericalNumber = request.CommericalNumber,
+                FaceBookAccout = request.FaceBookAccout,
+                InstagramAccount = request.InstagramAccount,
+                TwitterAccount = request.TwitterAccount,  
+                PaymentTerm = request.PaymentTerm,
+                TaxId = request.TaxId,
+            };
+
+            await companyRepository.AddCompany(company);
+
+            await userManager.AddToRoleAsync(user, "Company");
+            await signInManager.SignInAsync(user, isPersistent: false);
+            return CreateResponse(true, 201, new {user.Id, user.UserName, company.CompanyName, user.Email }, "Company registered successfully");
+
+        }
+    }
+}
