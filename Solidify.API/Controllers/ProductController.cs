f6308123f5d43970e459ee464465b9b556fc4906
ansumesse <manas8269@gmail.com>
2025-01-27 14:10:37 +0200

    Add CreateProduct Endpoint

---
+++ Solidify.API/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Solidify.Application.E_Commerce.Products.Commands.CreateProduct;
+
+namespace Solidify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(IMediator mediator) : BaseController(mediator)
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(CreateProductCommand command)
+        {
+            return await HandleCommand(command);
+        }
+    }
+}
