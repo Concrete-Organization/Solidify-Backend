a73b486b3ad8bf0a50f91d758a8fc34de3646c83
ansumesse <manas8269@gmail.com>
2025-01-27 18:48:59 +0200

    Modify specification to return Items Count

--- Solidify.Infrastructure/SpecificationsEvaluator.cs
+++ Solidify.Infrastructure/SpecificationsEvaluator.cs
@@ -6,10 +6,11 @@ namespace Solidify.Infrastructure
 {
     public static class SpecificationsEvaluator<TEntity> where TEntity : class
     {
-        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        public static (IQueryable<TEntity>, int) GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
         {
             var query = inputQuery;
-            
+            int totalCount = 0;
+
             if (spec.Criteria is not null)
                 query = query.Where(spec.Criteria);
 
@@ -23,9 +24,12 @@ namespace Solidify.Infrastructure
                 query = query.OrderByDescending(spec.SortDesc);
 
             if (spec.IsPaginationEnabled)
+            {
+                totalCount = query.Count();
                 query = query.Skip(spec.Skip).Take(spec.Take);
+            }
 
-            return query;
+            return (query, totalCount);
         }
     }
 }
