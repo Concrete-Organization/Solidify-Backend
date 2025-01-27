5e9cf13156de1580c5eba82f54f3c0cab6522041
ansumesse <manas8269@gmail.com>
2025-01-27 14:08:53 +0200

    Add ProductProfile

---
+++ Solidify.Application/E-Commerce/Products/Profiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Solidify.Application.E_Commerce.Products.Commands.CreateProduct;
+using Solidify.Domain.Entities.ECommerce;
+
+namespace Solidify.Application.E_Commerce.Products.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<CreateProductCommand, Product>();
+        }
+    }
+}
