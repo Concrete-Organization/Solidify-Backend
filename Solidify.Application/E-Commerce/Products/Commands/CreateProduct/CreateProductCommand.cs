af30b9c7c6bceb183f21cf5119acc51ef7d2deb7
ansumesse <manas8269@gmail.com>
2025-01-27 14:09:27 +0200

    Add Create Product Command & Handler

---
+++ Solidify.Application/E-Commerce/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Solidify.Application.Common.Dtos;
+using Solidify.Domain.Interfaces;
+
+namespace Solidify.Application.E_Commerce.Products.Commands.CreateProduct
+{
+    public class CreateProductCommand : IRequest<GeneralResponseDto>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Description { get; set; }
+        public string Measurement { get; set; }
+        public string Image { get; set; }
+        public int Rate { get; set; }
+        public int CategoryId { get; set; }
+        public int BrandId { get; set; }
+        public string CompanyId { get; set; }
+    }
+}
---
+++ Solidify.Application/E-Commerce/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Solidify.Application.Common;
+using Solidify.Application.Common.Dtos;
+using Solidify.Application.E_Commerce.Products.Commands.CreateProduct;
+using Solidify.Domain.Entities.ECommerce;
+using Solidify.Domain.Interfaces;
+
+namespace Solidify.Application.E_Commerce.Products.Commands
+{
+    public class CreateProductCommandHandler(IUnitOfWork unitOfWork,
+        IMapper mapper) 
+        : IRequestHandler<CreateProductCommand, GeneralResponseDto>
+    {
+        public async Task<GeneralResponseDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var productRepository = unitOfWork.GetRepository<Product>();
+
+            await productRepository.AddAsync(mapper.Map<Product>(request));
+            await unitOfWork.Commit();
+
+            return GeneralResponse.CreateResponse(true, StatusCodes.Status201Created, null,
+                "Product Created Successfully");
+        }
+    }
+}
