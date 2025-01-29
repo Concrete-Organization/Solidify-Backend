82701c6e74eac0d94c30693be2b1b67a5020a436
ansumesse <manas8269@gmail.com>
2025-01-29 14:05:08 +0200

    Add DeleteProductCommand

---
+++ Solidify.Application/E-Commerce/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Solidify.Application.Common;
+using Solidify.Application.Common.Dtos;
+using Solidify.Application.Files;
+using Solidify.Domain.Entities.ECommerce;
+using Solidify.Domain.Enums;
+using Solidify.Domain.Exceptions;
+using Solidify.Domain.Interfaces;
+using Solidify.Domain.Specification.ProductSpecifications;
+
+namespace Solidify.Application.E_Commerce.Products.Commands.DeleteProduct
+{
+    public class DeleteProductCommandHandler(IUnitOfWork unitOfWork,
+        IFileService fileService) : IRequestHandler<DeleteProductCommand, GeneralResponseDto>
+    {
+        public async Task<GeneralResponseDto> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var productRepository = unitOfWork.GetRepository<Product>();
+
+            var product = await productRepository.GetAsync(new ProductsSpecification(request.Id)) 
+                          ?? throw new NotFoundException(nameof(Product), request.Id);
+
+            if (!String.IsNullOrEmpty(product.ImageUri))
+            {
+                fileService.DeleteFile(product.ImageUri, FileType.ProductImage);
+            }
+
+            productRepository.Delete(product);
+            await unitOfWork.Commit();
+
+            return GeneralResponse.CreateResponse(true, StatusCodes.Status200OK, null,
+                $"Product with id [{request.Id}] deleted successfully");
+        }
+    }
+}
