c41ce23f5361e395f4ab68e93ee11000bc68cd6d
moustafanafeaa <moustafa.anafea@gmail.com>
2025-02-21 00:08:53 +0200

    add category controller

---
+++ Solidify.API/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Solidify.Application.E_Commerce.Categories.Queries;
+using Solidify.Application.E_Commerce.Categories.Query.GetAllCategories;
+
+namespace Solidify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController(IMediator mediator) : BaseController(mediator)
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllCategoriesQuery query)
+        {
+            return await HandleCommand(query);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return await HandleCommand(new GetCategoryQuery(id));
+        }
+
+    }
+}
