765e813551a02da979ee040ff311e42c9c45985b
ansumesse <manas8269@gmail.com>
2025-02-22 20:21:09 +0200

    Add Validatio on creating post

---
+++ Solidify.Application/Community/Posts/Commands/CreatePost/CreatePostCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Solidify.Application.Community.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandValidator : AbstractValidator<CreatePostCommand>
+    {
+        public CreatePostCommandValidator()
+        {
+            RuleFor(p => p.Content)
+                .MaximumLength(1000);
+
+            RuleFor(p => p.ImageUris)
+                .Must(p => p.Count <= 5)
+                .WithMessage("You can upload up to 5 images");
+        }
+    }
+}
