e1ffbf3f4466f53e74425523ca39645d996cfd9c
ansumesse <manas8269@gmail.com>
2025-02-25 03:20:43 +0200

    Add BadRequestException to ErrorHandlingMiddlewar

--- Solidify.API/Middlewares/ErrorHandlingMiddleware.cs
+++ Solidify.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using Solidify.Application.Common;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Solidify.Application.Common;
 using Solidify.Application.Common.Dtos;
 using Solidify.Domain.Exceptions;
 
@@ -17,6 +18,12 @@ public class ErrorHandlingMiddleware : IMiddleware
             context.Response.StatusCode = StatusCodes.Status404NotFound;
             await context.Response.WriteAsJsonAsync(GeneralResponse.CreateResponse(false, StatusCodes.Status404NotFound,
                 null, notFound.Message));
+        }
+        catch (BadRequestException e)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsJsonAsync(GeneralResponse.CreateResponse(false, StatusCodes.Status400BadRequest,
+                null, e.Message));
         }
         catch (Exception e)
         {
---
+++ Solidify.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,6 @@
+﻿namespace Solidify.Domain.Exceptions
+{
+    public class BadRequestException(string message): Exception(message)
+    {
+    }
+}
