e79fcde45f5028ccecb053da428447e2d2e8325d
ansumesse <manas8269@gmail.com>
2025-03-25 21:15:22 +0200

    Add ShippingAddress

---
+++ Solidify.API/Controllers/ShippingAddressController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Solidify.Application.E_Commerce.Brands.Commands.AddBrand;
+using Solidify.Application.E_Commerce.ShippingAddresses.Commands;
+
+namespace Solidify.API.Controllers
+{
+
+    public class ShippingAddressController(IMediator mediator) : BaseController(mediator)
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateShippingAddressCommand command)
+        {
+            return await HandleCommand(command);
+        }
+
+    }
+}
---
+++ Solidify.Application/E-Commerce/ShippingAddresses/Dtos/ShippingAddressDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Solidify.Application.E_Commerce.ShippingAddresses.Dtos
+{
+    public class ShippingAddressDto
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
